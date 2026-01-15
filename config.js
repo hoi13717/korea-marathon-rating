8c560d3607973ace2a7be4ff8fb3a43fd0bf7397
hoi <howon080@gmail.com>
2026-01-15 18:28:42 +0900

    index 레이아웃변경 및 config.js 연동

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+const CONFIG = {
+    SUPABASE_URL: 'https://qnnxsdhtzysbnjdlptpc.supabase.co',
+    SUPABASE_KEY: 'sb_publishable_lp-ODYK9ZFOXLWHQrX54kg_8UgNOWYg',
+    WEATHER_KEY: '하원님의_날씨_KEY_입력'
+};
